c7d00cea85c03cbc585ab4396025e577e9ef0f15
Noval Dwi Putra <nvldwiptr@gmail.com>
2020-10-28 06:15:33 +0700

    add relation model

--- app/User.php
+++ app/User.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use App\Model\Transaction\Loan;
+use App\Model\Transaction\TReturn;
 
 class User extends Authenticatable
 {
@@ -42,5 +43,10 @@ class User extends Authenticatable
     public function loan()
     {
         return $this->hasMany(Loan::class);
+    }
+
+    public function return()
+    {
+        return $this->hasMany(TReturn::class);
     }
 }
